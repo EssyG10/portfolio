c0c0a9ea48d1b194c320a1bdfe6a89668bc1ccdc
EssyG10 <88305000+EssyG10@users.noreply.github.com>
2021-09-23 13:04:24 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('#user-info #testimonials .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        560: {
+            items: 2
+        }
+    }
+});
+
+//hire button
+let BTN = document.getElementById("hireBtn");
+
+BTN.addEventListener("click", () => {
+    alert("Get in Touch To hire me 😊✌\n✉ Email :  hazardmburu10@gmail.com\n 📞 Telephone :  +254110026199")
+})
